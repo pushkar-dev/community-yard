0292848a1d46e2ef9dfe6e8ebb59cf0d0c834c53
PIYUSH VERMA <b20163@students.iitmandi.ac.in>
2022-02-26 11:36:05 +0530

    Starting files added

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-var ejs=require('ejs');
-var express = require('express');
-var app = express();
+require('dotenv').config();
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const express = require("express");
+const ejs = require("ejs")
+const mongoose = require("mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
+const _ = require('lodash');
+const moment = require("moment");
+require('https').globalAgent.options.rejectUnauthorized = false;
+const mongoStore = require("connect-mongo");
+const app = express();
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -9,7 +20,7 @@ app.set('view engine', 'ejs');
 
 // index page
 app.get('/', function(req, res) {
-  res.render('pages/index');
+  res.render('home');
 });
 
 // about page
@@ -17,5 +28,6 @@ app.get('/about', function(req, res) {
   res.render('pages/about');
 });
 
-app.listen(8080);
-console.log('Server is listening on port 8080');
+app.listen(process.env.PORT || 8080, function(){
+  console.log("Server running on port 8080" );
+});
