c611acd9de552298b0e64232c4290df31647db30
Vinayak Sachan <vinayakasachan@gmail.com>
2022-02-28 00:43:24 +0530

    heroku formalities2

--- app.js
+++ app.js
@@ -128,7 +128,7 @@ passport.use(
             clientID: process.env.CLIENT_ID,
             clientSecret: process.env.CLIENT_SECRET,
             // callbackURL: "https://community-scrapeyard.herokuapp.com/auth/google/CS",
-            callbackURL: "http://localhost:8080/auth/google/CS",
+            callbackURL: "https://community-yard.herokuapp.com/auth/google/CS",
             userProfileUrl: "https://www.googleapis.com.oauth2.v3.userinfo",
         },
         function(accessToken, refreshToken, profile, cb) {
