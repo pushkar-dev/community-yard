62ee4ea51bb20c6d6ff112e43761ae063e288f17
Rimurudemon <www.piyushdwivedi117@gmail.com>
2023-12-29 22:14:43 +0530

    image deletion bug fixed

--- routes/addItem.js
+++ routes/addItem.js
@@ -1,15 +1,17 @@
-const {Router}= require('express');
-const Item = require('../schema/item');
-const Chat= require('../schema/message');
+const { Router } = require("express");
+const Item = require("../schema/item");
+const Chat = require("../schema/message");
 var upload = require("../src/storage");
 const addRoute = Router();
+const fs = require("fs");
+const path = require("path");
 
 addRoute.get("/newAdd", function (req, res) {
-    if (req.isAuthenticated()) {
-      res.render("addform", { user: req.user });
-    } else {
-      res.redirect("/");
-    }
+  if (req.isAuthenticated()) {
+    res.render("addform", { user: req.user });
+  } else {
+    res.redirect("/");
+  }
 });
 
 addRoute.post("/addItem", upload.single("image"), function (req, res) {
@@ -58,29 +60,64 @@ addRoute.post("/deleteAd", function (req, res) {
   if (req.isAuthenticated()) {
     if (req.user.email === req.body.owner_email) {
       const body = req.body;
-      Chat.deleteOne(
-        { item_name: body.item_name, owner_email: body.owner_email },
-        function (err, res) {
+
+      // Find the item in the database
+      Item.findOne(
+        {
+          item_name: body.item_name,
+          owner_email: body.owner_email,
+        },
+        function (err, item) {
           if (err) console.log(err);
           else {
-            console.log("Chat deleted");
+            // Delete the image file
+            const filePath = path.join(
+              __dirname,
+              "..",
+              "uploaded_docs",
+              item.upload
+            );
+            console.log("Deleting file:", filePath);
+            fs.unlink(filePath, function (err) {
+              if (err) {
+                console.log("Error deleting file:", err);
+              } else {
+                console.log("Image file deleted");
+              }
+            });
+            // Delete the chat
+            Chat.deleteOne(
+              {
+                item_name: body.item_name,
+                owner_email: body.owner_email,
+              },
+              function (err, res) {
+                if (err) console.log(err);
+                else console.log("Chat deleted");
+              }
+            );
+
+            // Delete the item
+            Item.deleteOne(
+              {
+                item_name: body.item_name,
+                owner_email: body.owner_email,
+              },
+              function (err, res) {
+                if (err) console.log(err);
+                else console.log("Item deleted");
+              }
+            );
           }
         }
       );
-      Item.deleteOne(
-        { item_name: body.item_name, owner_email: body.owner_email },
-        function (err, res) {
-          if (err) console.log(err);
-          else {
-            console.log("Item Deleted successfully");
-          }
-        }
-      );
-      res.redirect("/fetchForOwner");
-    } 
-    else {
-      res.redirect("/fetchForBuyer");
+
+      res.redirect("/");
+    } else {
+      res.redirect("/");
     }
+  } else {
+    res.redirect("/");
   }
 });
 
