af3e55850da14d8e56648e0b143c377e12b0e8ce
AkshatBujethiya <akshatmeena2711@gmail.com>
2024-01-26 15:24:16 +0530

    Add basic user info in profile page

--- routes/profile.js
+++ routes/profile.js
@@ -1,9 +1,22 @@
 const { Router } = require('express');
 const profileRoute = Router();
+const User = require('../schema/user');
 
 profileRoute.get('/profile/:username', (req, res) => {
+  if (req.isAuthenticated()) {
   username = req.params.username;
-  res.render('profile',{user: req.user});
+  User.findOne({ username: username }, (err,found)=>{
+    if (err) {
+      console.log(err);
+      res.redirect('/');
+    }
+    else{
+      res.render('profile', {user: found});
+    }
+  });
+} else {
+  res.redirect('/');
+}
 });
 
 module.exports = profileRoute;
