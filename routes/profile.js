ac6e0da80f1f2d7b931a2aedeebbcf428dfba822
AkshatBujethiya <akshatmeena2711@gmail.com>
2024-01-27 18:07:07 +0530

    Add items in user profile

--- routes/profile.js
+++ routes/profile.js
@@ -1,21 +1,31 @@
 const { Router } = require('express');
 const profileRoute = Router();
 const User = require('../schema/user');
+const Item = require('../schema/item');
 
 profileRoute.get('/profile/:username', (req, res) => {
   if (req.isAuthenticated()) {
-  username = req.params.username;
-  User.findOne({ username: username }, (err,found)=>{
-    if (err) {
-      console.log(err);
-      res.redirect('/');
-    }
-    else{
-      res.render('profile', {user: found});
-    }
-  });
+    username = req.params.username;
+    User.findOne({ username: username }, (err,found)=>{
+      if (err) {
+        console.log(err);
+        res.redirect('/');
+      }
+      else {
+        Item.find({ owner_email: found.email }, (err,items)=>{
+          if (err) {
+            console.log(err);
+            res.redirect('/');
+          }
+          else {
+            console.log(items);
+            res.render('profile', {profile_user: found, items: items, user: req.user});
+          }
+        });
+      }
+    });
 } else {
-  res.redirect('/');
+    res.redirect('/');
 }
 });
 
