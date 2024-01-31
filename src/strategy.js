9feac833985177c4396fb0f671cad8925ced0b6b
Pushkar Patel <pushkar.umrao@gmail.com>
2024-01-31 10:41:44 +0530

    redirect uri updated

--- src/strategy.js
+++ src/strategy.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 const User = require("../schema/user");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+const callbackuri = process.env.NODE_ENV === "production" ? process.env.CALLBACK_URL:"http://localhost:8080/auth/google/CS";
 const strategy = new GoogleStrategy(
     {
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
       // callbackURL: "https://community-scrapeyard.herokuapp.com/auth/google/CS",
-      callbackURL: "http://localhost:8080/auth/google/CS",
+      callbackURL: callbackuri,
       userProfileUrl: "https://www.googleapis.com.oauth2.v3.userinfo",
     },
     function (accessToken, refreshToken, profile, cb) {
